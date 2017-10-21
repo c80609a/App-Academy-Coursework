0097228a3038e7a3b3a6fe33f30c0f607a958974
App Academy Student <student@appacademy.io>
2017-10-20 17:46:41 -0700

    start searchable spec

--- ActiveRec1/lib/02_searchable.rb
+++ ActiveRec1/lib/02_searchable.rb
@@ -1,12 +1,35 @@
 require_relative 'db_connection'
 require_relative '01_sql_object'
+require 'byebug'
 
 module Searchable
   def where(params)
-    # ...
+    # debugger
+    vals = params.values
+    where_line = params.keys.map do |key|
+      "#{key} = ?"
+    end
+    if params.keys.count > 1
+      where_line.join(' AND ')
+    else
+      where_line.to_s
+    end
+
+    data = DBConnection.execute(<<-SQL, *vals)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_line}
+    SQL
+    return nil if data.first.empty?
+    parse_all(data)
+
   end
 end
 
 class SQLObject
   # Mixin Searchable here...
+  extend Searchable
 end
