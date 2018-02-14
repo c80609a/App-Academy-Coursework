a6bb3e6ca69a0afbd22bdb3baf6601b89c47666d
Christina Kiesz <ckiesz@gmail.com>
2018-02-14 15:52:28 -0800

    Finish frog_hops on dynamic programming and add in solutions for easier learning

---
+++ Review/W1D2/match2/board.rb
@@ -0,0 +1,18 @@
+class Board
+    def initialize
+        
+    end
+
+    def populate
+    end
+
+    def render
+    end
+
+    def won?
+    end
+
+    def reveal
+    end
+
+end
--- W9+/Christina_Kiesz_Dynamic/lib/dynamic_programming.rb
+++ W9+/Christina_Kiesz_Dynamic/lib/dynamic_programming.rb
@@ -1,51 +1,88 @@
 class DynamicProgramming
 
   def initialize
-    @cache = {}
+    @frog_cache = []
   end
 
-  def blair_nums(n)
-    @cache[1] = 1
-    @cache[2] = 2
-    return @cache[n] if @cache[n]
-    odd = (n-1) * 2 - 1
-    @cache[n] = blair_nums(n-2) + blair_nums(n-1) + odd
-    return @cache[n]
-  end
+  # def blair_nums(n)
+  #   @cache[1] = 1
+  #   @cache[2] = 2
+  #   return @cache[n] if @cache[n]
+  #   odd = (n-1) * 2 - 1
+  #   @cache[n] = blair_nums(n-2) + blair_nums(n-1) + odd
+  #   return @cache[n]
+  # end
+
+## FROGS
+
 
   def frog_hops_bottom_up(n)
-    cache = frog_cache_builder(n)
-    return cache[n]
+    cached = frog_cache_builder(n)
+
+    return cached[n]
   end
+
 
   def frog_cache_builder(n)
     # can jump 1, 2, or 3
-
-    @cache[1] = [[1]]
-    @cache[2] = [[1, 1], [2]]
-    @cache[3] = [[1 ,1 ,1], [1, 2], [2, 1], [3]]
-    return @cache if n <= 3
-
+    ways = [[[]], [[1]], [[1, 1], [2]], [[1, 1, 1], [1, 2], [2, 1], [3]]]
+    return ways if n < 3
 
     (4..n).each do |i|
-      (1..3).each do |j|
-        @cache[i] = []
-        
+      new_way_set = []
+      (1..3).each do |first_hop|
+        ways[i - first_hop].each do |way|
+          new_way = [first_hop]
+          way.each do |hop|
+            new_way << hop
+          end
+          new_way_set << new_way
+        end
       end
+      ways << new_way_set
     end
-
-
-
-    @cache
+    ways
   end
 
   def frog_hops_top_down(n)
-
+    @frog_cache = [[[]], [[1]], [[1,1], [2]]]
+    frog_hops_top_down_helper(n)
   end
 
   def frog_hops_top_down_helper(n)
-
+    return @frog_cache[n] if @frog_cache[n]
+    new_way_set = []
+    (1..3).each do |first_step|
+      frog_hops_top_down_helper(n - first_step).each do |way|
+        new_way = [first_step]
+        way.each do |step|
+          new_way << step
+        end
+        new_way_set << new_way
+      end
+    end
+    @frog_cache[n] = new_way_set
   end
+  # def frog_hops_top_down(n)
+  #   @cache = [[[]], [[1]], [[1,1], [2]]]
+  #   frog_hops_top_down_helper(n)
+  # end
+  #
+  # def frog_hops_top_down_helper(n)
+  #   return @cache[n] if @cache[n]
+  #
+  #   new_way_set = []
+  #   (1..3).each do |first_hop|
+  #     frog_hops_top_down_helper(n - first_hop).each do |way|
+  #       new_way = [first_hop]
+  #       way.each do |hop|
+  #         new_way << hop
+  #       end
+  #       new_way_set << new_way
+  #     end
+  #   end
+  #   @cache[n] = new_way_set
+  # end
 
   def super_frog_hops(n, k)
 
