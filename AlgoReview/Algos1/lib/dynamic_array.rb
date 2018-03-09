c2e2678efb1aa9a62666a09da773c5e7909c9556
Christina Kiesz <ckiesz@gmail.com>
2018-03-08 17:00:53 -0800

    Finish dynamic arrays, start ringbuffer

--- AlgoReview/Algos1/lib/dynamic_array.rb
+++ AlgoReview/Algos1/lib/dynamic_array.rb
@@ -4,29 +4,29 @@ class DynamicArray
   attr_reader :length
 
   def initialize
-    @store = StaticArray.new(8)
-    @capacity = 8
-    @length = 0
+    self.store = StaticArray.new(8)
+    self.capacity = 8
+    self.length = 0
   end
 
   # O(1)
   def [](index)
     check_index(index)
-    @store[index]
+    store[index]
   end
 
   # O(1)
   def []=(index, value)
     check_index(index)
-    @store[index] = value
+    store[index] = value
   end
 
   # O(1)
   def pop
-    raise "index out of bounds" if length == 0
-    value = @store[length - 1]
-    @store[length - 1] = nil
-    @length -= 1
+    raise "index out of bounds" unless (length > 0)
+    value = self[length - 1]
+    self[length - 1] = nil
+    self.length -= 1
     value
   end
 
@@ -34,31 +34,35 @@ class DynamicArray
   # resize.
   def push(val)
     resize! if length == capacity
-    @store[@length] = val
-    @length += 1
+    # seems like you have to increase the length first to avoid index err
+    self.length += 1
+    self[length - 1] = val
+
     val
   end
 
   # O(n): has to shift over all the elements.
   def shift
-    raise 'index out of bounds' if @length == 0
-    value = @store[0]
-    (0...length).each do |i|
-      @store[i] = @store[i+1]
+    raise 'index out of bounds' if length == 0
+    value = self[0]
+    (1...length).each do |i|
+      self[i-1] = self[i]
     end
-    @length -= 1
-    puts @store
+    self.length -= 1
+
     value
   end
 
   # O(n): has to shift over all the elements.
   def unshift(val)
     resize! if length == capacity
-    (length..1).each do |i|
-      @store[length] = @store[length - 1]
+    # have to add an extra to length first
+    self.length += 1
+    (length - 2).downto(0).each do |i|
+      self[i + 1] = self[i]
     end
-    @store[0] = val
-    @length += 1
+    self[0] = val
+
     val
   end
 
@@ -67,13 +71,15 @@ class DynamicArray
   attr_writer :length
 
   def check_index(index)
-    raise "index out of bounds" if index >= length || index < 0
+    raise "index out of bounds" unless (index >= 0) && (index < length)
   end
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
-
-
-
+    new_cap = capacity * 2
+    new_store = StaticArray.new(new_cap)
+    length.times { |i| new_store[i] = self[i] }
+    self.capacity = new_cap
+    self.store = new_store
   end
 end
--- AlgoReview/Algos1/lib/ring_buffer.rb
+++ AlgoReview/Algos1/lib/ring_buffer.rb
@@ -4,22 +4,40 @@ class RingBuffer
   attr_reader :length
 
   def initialize
+    self.store = StaticArray.new(8)
+    self.capacity = 8
+    self.length = 0
   end
 
   # O(1)
   def [](index)
+    check_index(index)
+    self[index]
   end
 
   # O(1)
   def []=(index, val)
+    check_index(index)
+    self[index] = val
   end
 
   # O(1)
   def pop
+    raise "index out of bounds" if (length == 0)
+    val = self[length -1]
+
+    self[length - 1] = nil
+    self.length -= 1
+
+    val
   end
 
   # O(1) ammortized
   def push(val)
+    resize! if length == capacity
+    self.length += 1
+    self[length - 1] = val
+
   end
 
   # O(1)
@@ -35,6 +53,7 @@ class RingBuffer
   attr_writer :length
 
   def check_index(index)
+    raise "index out of bounds" unless index >= 0 && index < length
   end
 
   def resize!
