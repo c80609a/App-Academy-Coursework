39c6bc58fc64bd3ab79cafeeeb1e6cd687f3bd11
Christina Kiesz <ckiesz@gmail.com>
2018-03-24 14:52:21 -0700

    Finish hashing

--- AlgoReview/Algos2/lib/p01_int_set.rb
+++ AlgoReview/Algos2/lib/p01_int_set.rb
@@ -1,22 +1,32 @@
 class MaxIntSet
   def initialize(max)
+    @store = Array.new(max) { false }
+    @max = max
   end
 
   def insert(num)
+    validate!(num)
+    @store[num] = true
   end
 
   def remove(num)
+    validate!(num)
+    @store[num] = false
   end
 
   def include?(num)
+    validate!(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+    return (num >= 0) && (num <= @max)
   end
 
   def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
   end
 end
 
@@ -27,18 +37,23 @@ class IntSet
   end
 
   def insert(num)
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    index = num % num_buckets
+    @store[index]
   end
 
   def num_buckets
@@ -55,18 +70,25 @@ class ResizingIntSet
   end
 
   def insert(num)
+    resize! if num_buckets == count
+    self[num] << num
+    @count += 1
   end
 
   def remove(num)
+    self[num].delete(num)
+    @count -= 1
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -74,5 +96,11 @@ class ResizingIntSet
   end
 
   def resize!
+    old_num = num_buckets
+    old_store = @store.flatten
+    @store = nil
+    @count = 0
+    @store = Array.new(old_num  * 2) { [] }
+    old_store.each {|num| insert(num) }
   end
 end
