8a1b2073d0b4af4fdb5999e27d7ddfe7402fe803
Christina Kiesz <ckiesz@gmail.com>
2018-04-02 17:31:07 -0700

    Finish intro-debugging

--- AAO/aao_intro_debug/lib/exercises.rb
+++ AAO/aao_intro_debug/lib/exercises.rb
@@ -9,8 +9,8 @@ LITTLE_WORDS = [
 
 def titleize(title)
   words = title.split(" ")
-  titleized_words = words.map_with_index do |word, i|
-    unless (idx == 0 && LITTLE_WORDS.include(word)
+  titleized_words = words.map.with_index do |word, i|
+    if i != 0 && LITTLE_WORDS.include?(word)
       word.downcase
     else
       word.capitalize
@@ -24,15 +24,16 @@ end
 # Write a method that returns the largest prime factor of a given integer.
 
 def prime?(num)
-  (2..num).none? { |factor| num % factor == 0 }
+  (2...num).none? { |factor| num % factor == 0 }
 end
 
 def largest_prime_factor(num)
   num.downto(2) do |factor|
     if (num % factor).zero?
-      return factor if prime(factor)
+      return factor if prime?(factor)
     end
   end
+  nil
 end
 
 
@@ -41,12 +42,12 @@ end
 # Only include substrings of length > 1.
 
 def symmetric_substrings(str)
-  symm_subs = ""
+  symm_subs = []
 
   str.length.times do |start_pos|
-    (2..(length - start_pos).each do |len|
+    (2..(str.length - start_pos)).each do |len|
       substr = str[start_pos...(start_pos + len)]
-      symm_subs << substr if substr = substr.reverse
+      symm_subs << substr if substr == substr.reverse
     end
   end
 
@@ -56,8 +57,8 @@ end
 
 # Write a method that returns `true` if all characters in the string
 # are unique and `false` if they are not.
-
+#
 def all_unique_chars?(str)
-  letters = str.split('').reject{ |char| char == '  '}
+  letters = str.split('').reject { |char| char == '  '}
   letters.uniq.length = letters.length
 end
