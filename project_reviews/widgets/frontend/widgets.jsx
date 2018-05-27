c4d759b01be68fd3c9876808031e99f6aa6aca5e
Christina Kiesz <ckiesz@gmail.com>
2018-05-27 16:39:18 -0700

    Working webpack with jsx entry point

---
+++ project_reviews/widgets/frontend/widgets.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import Filename from 'file';
+
+document.addEventListener('DOMContentLoaded', function () {
+  ReactDOM.render(<div>"Hi from Entry Point"</div>, document.getElementById('main'));
+});
