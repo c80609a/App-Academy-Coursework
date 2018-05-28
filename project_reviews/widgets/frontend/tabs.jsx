5669875dbda19f81f4dca8d15a72ce2b9d56ad2e
Christina Kiesz <ckiesz@gmail.com>
2018-05-27 23:55:22 -0700

    Creating tabs divs

--- project_reviews/widgets/frontend/tabs.jsx
+++ project_reviews/widgets/frontend/tabs.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
 class Tabs extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedTab: 0
+        };
+        this.selectTab = this.selectTab.bind(this);
+    }
 
+    selectTab (num) {
+        this.setState({ selectedTab: num });
+    }
+
+    render () {
+        let pane = this.props.panes[this.state.selectedTab];
+
+        return (
+            <div>
+                <h1>Tabs</h1>
+                <div className="tabs-container">PANES</div>
+            </div>
+                    
+        );
+    }
 }
 
 export default Tabs;
