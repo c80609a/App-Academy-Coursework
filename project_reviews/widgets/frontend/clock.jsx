551a898d0f6c0836c201b4e618872867714d3974
Christina Kiesz <ckiesz@gmail.com>
2018-05-27 22:47:17 -0700

    Finish clock and styling, start tabs widget

--- project_reviews/widgets/frontend/clock.jsx
+++ project_reviews/widgets/frontend/clock.jsx
@@ -28,13 +28,17 @@ class Clock extends React.Component {
     let mins = this.state.now.getMinutes();
     let secs = this.state.now.getSeconds();
 
-    hours = (hours < 10) ? '0${hours}' : hours;
-    mins = (mins < 10) ? '0${mins}' : mins;
-    secs = (secs < 10) ? '0${secs}' : secs;
+    hours = (hours < 10) ? `0${hours}` : hours;
+    mins = (mins < 10) ? `0${mins}` : mins;
+    secs = (secs < 10) ? `0${secs}` : secs;
+    // secs = `0${secs}`;
 
     return (
-      <div><span>{hours}:{mins}:{secs}</span>
-        
+      <div className="clock-widget">
+        <div className="clock-container">
+          <span className="time-container">{hours}:{mins}:{secs}</span>
+          <span className="date-container">{this.state.now.toLocaleDateString()}</span>
+        </div>
       </div>
       );
     }
