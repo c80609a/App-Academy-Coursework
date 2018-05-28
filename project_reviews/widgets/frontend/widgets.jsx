609fd9c4b02e1eb9b9e87e3b617d4ef5119a0b84
Christina Kiesz <ckiesz@gmail.com>
2018-05-27 22:54:37 -0700

    Change to a functional root to hold components in widgets.jsx

--- project_reviews/widgets/frontend/widgets.jsx
+++ project_reviews/widgets/frontend/widgets.jsx
@@ -2,6 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Clock from './clock';
+import Tabs from './tabs';
+
+const Panes = [
+  {title: 'Tab 1', content: "The mighty first."},
+  {title: 'Tab 2', content: "The middle second."},
+  {title: 'Tab 3', content: "Third and last."}
+];
+
+function Root() {
+  return(
+    <div>
+      <Clock />
+      <Tabs panes={Panes} />
+    </div>
+  )
+}
 
 document.addEventListener('DOMContentLoaded', function () {
   ReactDOM.render(<Clock />, document.getElementById("main"));
